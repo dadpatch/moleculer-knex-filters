0203da6641eb62fbc9bf9b462acabaa7e3503242
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2022-09-23 13:35:58 +0300

    parse not only and but $or as well

--- index.js
+++ index.js
@@ -1,80 +1,100 @@
-'use strict';
+"use strict";
 
-const _ = require('lodash');
+const _ = require("lodash");
 const Validator = require("fastest-validator");
 const validator = new Validator({
-	useNewCustomCheckerFunction: true
+  useNewCustomCheckerFunction: true,
 });
 
 module.exports = function filtersMixin(opts = {}) {
-
   const parseFilterObj = (ctx) => {
-    if (!ctx.params.filter) return ctx
-    ctx.params.filter = ctx.params.filter || {}
-    ctx.params.query = ctx.params.query || {}
+    if (!ctx.params.filter) return ctx;
+    ctx.params.filter = ctx.params.filter || {};
+    ctx.params.query = ctx.params.query || {};
 
     if (typeof ctx.params.filter === "string") {
-      ctx.params.filter = JSON.parse(ctx.params.filter)
+      ctx.params.filter = JSON.parse(ctx.params.filter);
     }
     if (typeof ctx.params.query === "string") {
-      ctx.params.query = JSON.parse(ctx.params.query)
+      ctx.params.query = JSON.parse(ctx.params.query);
     }
 
-    ctx.params.query.$and = ctx.params.query.$and || []
-    Object.keys(ctx.params.filter).forEach((key) => {
-      let field = ctx.service?.settings?.fields?.[key]
-      if (typeof field === "string") field = validator.parseShortHand(field);
+    const translateToQuery = (obj) => {
+      const querySearch = [];
+      Object.entries(obj).forEach(([key, value]) => {
+        let field = ctx.service?.settings?.fields?.[key];
+        if (typeof field === "string") field = validator.parseShortHand(field);
 
-      if (field) {
-        const typeOfField = field.type
+        if (field) {
+          const typeOfField = field.type;
 
-        let querySearch = {}
-        const columnName = field.columnName || key
-        const value = ctx.params.filter[key]
-        if (typeOfField === "string" && value && typeof value === "string") {
-          querySearch[key] = {
-            $raw: {
-              condition: `${_.snakeCase(columnName)} ilike ?`,
-              bindings: [`%${value}%`]
-            }
-          } 
-        } else if (value || typeof value === "boolean") {
-          querySearch[key] = value
+          let querySearch = {};
+          const columnName = field.columnName || key;
+          if (typeOfField === "string" && value && typeof value === "string") {
+            querySearch[key] = {
+              $raw: {
+                condition: `${_.snakeCase(columnName)} ilike ?`,
+                bindings: [`%${value}%`],
+              },
+            };
+          } else if (value || typeof value === "boolean") {
+            querySearch[key] = value;
+          }
+
+          if (Object.keys(querySearch).length && !field.virtual) {
+            querySearch.push(querySearch);
+          }
         }
+      });
 
-        if (Object.keys(querySearch).length && !field.virtual) {
-          ctx.params.query.$and.push(querySearch)
-        }
-      } 
-    })
+      return querySearch;
+    };
 
-    return ctx
-  }
+    if (ctx.params.filter.$or) {
+      const orQuery = translateToQuery(orBindings);
+      delete ctx.params.filter.$or;
+      ctx.params.query.$or = ctx.params.query.$or || [];
+      ctx.params.query.$or.push(...orQuery);
+    }
+
+    const andQuery = translateToQuery(ctx.params.filter);
+    ctx.params.query.$and = ctx.params.query.$and || [];
+    ctx.params.query.$and.push(...andQuery);
+
+    return ctx;
+  };
 
   const beforeHooks = {
     list: parseFilterObj,
     count: parseFilterObj,
-    find: parseFilterObj
-  }
+    find: parseFilterObj,
+  };
 
   const schema = {
     merged(schema) {
       if (schema.hooks && schema.hooks.before) {
         Object.keys(beforeHooks).forEach((hook) => {
-          const currentHooks = Array.isArray(beforeHooks[hook]) ? beforeHooks[hook] : [ beforeHooks[hook] ]
+          const currentHooks = Array.isArray(beforeHooks[hook])
+            ? beforeHooks[hook]
+            : [beforeHooks[hook]];
           if (schema.hooks.before[hook]) {
-            const previousHooks = Array.isArray(schema.hooks.before[hook]) ? schema.hooks.before[hook] : [ schema.hooks.before[hook] ]
-            schema.hooks.before[hook] = [...previousHooks, ...currentHooks]
+            const previousHooks = Array.isArray(schema.hooks.before[hook])
+              ? schema.hooks.before[hook]
+              : [schema.hooks.before[hook]];
+            schema.hooks.before[hook] = [...previousHooks, ...currentHooks];
           } else {
-            schema.hooks.before[hook] = currentHooks
+            schema.hooks.before[hook] = currentHooks;
           }
-        })
+        });
       } else {
-        schema.hooks = schema.hooks || {}
-        schema.hooks.before = _.defaultsDeep(schema.hooks.before || {}, beforeHooks)
+        schema.hooks = schema.hooks || {};
+        schema.hooks.before = _.defaultsDeep(
+          schema.hooks.before || {},
+          beforeHooks
+        );
       }
-    }
+    },
   };
 
   return schema;
-}
+};
